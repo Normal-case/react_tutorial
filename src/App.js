e2a6b3c4888fb5e874232795571abc69b6de4896
Normal-case <8282chan94@gmail.com>
2021-07-15 22:05:44 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import './App.css';
+import React, { Component } from 'react';
+import TOC from "./component/TOC";
+import Content from "./component/Content";
+import Subject from './component/Subject';
+
+class App extends Component {
+  // 초기화
+  constructor(props){
+    super(props);
+    this.state = {
+      mode:'read',
+      subject:{title:'WEB', sub:'World Wide Web!'},
+      welcome:{title:'Welcome', desc:'Hello, React!!'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'},
+      ]
+    }
+  }
+  render() {
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === 'read'){
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
+    return (
+      <div className="App">
+        {/* <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>  */}
+        <header>
+          <h1><a href="/" onClick={function(e){
+            e.preventDefault(); // a 태그의 기본적인 동작방법을 방지
+            this.setState({
+              mode:'welcome'
+            });
+          }.bind(this)}>{this.state.subject.title}</a></h1>
+          {this.state.subject.sub}
+        </header>
+        <TOC data={this.state.contents}></TOC>
+        <Content title={_title} desc={_desc}></Content>
+      </div>
+    );
+  }
+}
+
+export default App;
